f10de4cdd2148ae26f1f93060b442d99e2edb9ad
Kinga <Ma3511ki>
2022-01-18 09:45:15 +0100

    use announcement component from flotiq components

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Announcement as FlotiqAnnouncement } from 'flotiq-components-react';
 
 const Announcement = ({ content }) => (
     <div className="max-w-7xl mt-12 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8">
-        <div className="rounded-lg flex justify-center items-center
-        bg-gradient-to-r from-turquoise to-secondary px-12 py-6"
-        >
-            <h2 className="text-center text-xl md:text-2xl leading-8 md:leading-10">
-                {content}
-            </h2>
-        </div>
+        <FlotiqAnnouncement
+            content={(
+                <h2 className="md:text-2xl leading-8 md:leading-10">
+                    {content}
+                </h2>
+            )}
+            additionalClasses={['bg-gradient-to-r', 'from-turquoise', 'to-secondary', 'rounded-lg']}
+            textAlignment="center"
+            textSize="xl"
+        />
     </div>
 );
 
