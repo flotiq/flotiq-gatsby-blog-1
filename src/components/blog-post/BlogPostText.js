ece59c927f0dc6cdbbcf7081dec29d4a0da265cc
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-01-19 10:37:48 +0100

    text spacing adjusted

--- src/components/blog-post/BlogPostText.js
+++ src/components/blog-post/BlogPostText.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const BlogPostText = ({textContent, additionalClass}) => (
-  <p className={['px-12 text-sm md:text-lg', ...additionalClass].join(' ')}>
-    {textContent}
-  </p>
+const BlogPostText = ({ textContent, additionalClass }) => (
+    <p className={['px-4 md:px-12 text-sm md:text-lg', ...additionalClass].join(' ')}>
+        {textContent}
+    </p>
 );
 
 export default BlogPostText;
