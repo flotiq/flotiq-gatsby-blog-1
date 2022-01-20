1332a8d65e47ec8f8cb815237046823e51cd19e7
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-01-20 10:06:33 +0100

    audio name spacing adjusted

--- src/components/blog-post/BlogPostAudio.js
+++ src/components/blog-post/BlogPostAudio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BlogPostAudio = ({ audioFile, additionalClass, audioName }) => (
     <div className={['flex items-center px-6 md:px-12 w-full md:w-3/5 mx-auto', ...additionalClass].join(' ')}>
-        <p className="mr-10">{audioName}</p>
+        <p className="mr-5 md:mr-10">{audioName}</p>
         <audio controls className="w-full">
             <source src={audioFile} type="audio/ogg" />
             <source src={audioFile} type="audio/mpeg" />
