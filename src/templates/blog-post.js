55de8d2840abd6b21e8b4070fb1155e0403c8052
Kinga <Ma3511ki>
2022-01-20 12:32:51 +0100

    fix code formatting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-  import Layout from '../layouts/layout';
-import BlogPostText from "../components/blog-post/BlogPostText";
+import Layout from '../layouts/layout';
+import BlogPostText from '../components/blog-post/BlogPostText';
 
 const BlogPostTemplate = ({ data, pageContext }) => {
     const post = data.blogpost;
     return (
         <Layout>
-          <div className="max-w-7xl mx-auto mt-20 pb-4 rounded-2xl bg-light-gray overflow-hidden">
-            <BlogPostText additionalClass={['py-6']} textContent="Flotiq strongly encourages separation of concerns and isolates the data from the presentation layer (unlike traditional CMS like WordPress). Thanks to that - you can easily use your data anywhere you need them, e.g.: "/>
-          </div>
+            <div className="max-w-7xl mx-auto mt-20 pb-4 rounded-2xl bg-light-gray overflow-hidden">
+                <BlogPostText
+                    additionalClass={['py-6']}
+                    textContent="Flotiq strongly encourages separation of concerns and isolates the data from the
+                     presentation layer (unlike traditional CMS like WordPress). Thanks to that - you can easily use
+                      your data anywhere you need them, e.g.: "
+                />
+            </div>
         </Layout>
     );
 };
