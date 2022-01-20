94d9a9805df5f64f51044bd36d0f5615a49a66a0
Kinga <Ma3511ki>
2022-01-20 16:14:48 +0100

    code format, change turquoise to primary

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Announcement as FlotiqAnnouncement} from 'flotiq-components-react';
+import { Announcement as FlotiqAnnouncement } from 'flotiq-components-react';
 
 const Announcement = ({ content }) => (
     <div className="max-w-7xl mt-12 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8">
@@ -9,7 +9,7 @@ const Announcement = ({ content }) => (
                     {content}
                 </h2>
             )}
-            additionalClasses={['bg-gradient-to-r', 'from-turquoise', 'to-secondary']}
+            additionalClasses={['bg-gradient-to-r', 'from-primary', 'to-secondary']}
             textAlignment="center"
             textSize="xl"
             rounded="lg"
