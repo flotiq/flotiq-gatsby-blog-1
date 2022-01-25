5d5a4c78f90a050a51c45b0a9be463cd559a7499
trzcina <michal.chrzaszcz@wp.pl>
2022-01-25 16:32:22 +0100

    Fix text-color

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -5,7 +5,7 @@ const Announcement = ({ content }) => (
     <div className="max-w-7xl mt-12 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8">
         <FlotiqAnnouncement
             content={(
-                <h2 className="md:text-2xl leading-8 md:leading-10">
+                <h2 className="md:text-2xl leading-8 md:leading-10 text-black">
                     {content}
                 </h2>
             )}
