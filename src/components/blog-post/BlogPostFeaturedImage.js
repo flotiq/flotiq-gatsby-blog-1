65a708f2e458579f78092b0d310253f625543a43
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-01-27 09:16:48 +0100

    Hotfix: Post featured image fix

--- src/components/blog-post/BlogPostFeaturedImage.js
+++ src/components/blog-post/BlogPostFeaturedImage.js
@@ -3,7 +3,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const BlogPostFeaturedImage = ({ headerImage, title }) => (
     <div className="pt-12 bg-gradient-to-r from-primary to-secondary rounded-xl">
-        {headerImage && (
+        {headerImage && headerImage[0] && (
             <GatsbyImage
                 alt={title}
                 image={getImage(headerImage[0].localFile)}
